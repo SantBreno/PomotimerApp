9edb3f333d9047152fde2adbd1c9e41684f9ae7d
SantBreno <brenosantsousa4@outlook.com>
2022-09-05 20:44:53 -0300

    Pomotimer-App

---
+++ app/src/main/java/com/projetos/pomotimer/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.projetos.pomotimer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private val todoFragment = TodoFragment()
+    private val configFragment = ConfigFragment()
+    private val timerFragment = TimerFragment()
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        replaceFragment(todoFragment)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.nav_todo -> replaceFragment(todoFragment)
+                R.id.nav_config -> replaceFragment(configFragment)
+                R.id.nav_timer -> replaceFragment(timerFragment)
+            }
+            true
+        }
+
+
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.body_container, fragment)
+        transaction.commit()
+    }
+}
+
